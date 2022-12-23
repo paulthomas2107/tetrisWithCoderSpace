f894fba285f8ddd2f8e9dab4690dbd85a2239fd8
Paul Thomas <paulthomas2107@gmail.com>
2022-12-23 13:14:55 +0000

    More....

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from settings import *
+from tetris import Tetris
 import sys
-import pygame as pg
 
 
 class App:
@@ -9,12 +9,15 @@ class App:
         pg.display.set_caption("Tetris...")
         self.screen = pg.display.set_mode(FIELD_RES)
         self.clock = pg.time.Clock()
+        self.tetris = Tetris(self)
 
     def update(self):
+        self.tetris.update()
         self.clock.tick(FPS)
 
     def draw(self):
         self.screen.fill(color=FIELD_COLOR)
+        self.tetris.draw()
         pg.display.flip()
 
     def check_events(self):
---
+++ tetris.py
@@ -0,0 +1,24 @@
+from settings import *
+import math
+from tetromino import Tetromino
+
+
+class Tetris:
+    def __init__(self, app):
+        self.app = app
+        self.sprite_group = pg.sprite.Group()
+        self.tetromino = Tetromino(self)
+
+    def draw_grid(self):
+        for x in range(FIELD_W):
+            for y in range(FIELD_H):
+                pg.draw.rect(self.app.screen, 'black',
+                             (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
+    def update(self):
+        self.tetromino.update()
+
+    def draw(self):
+        self.draw_grid()
+        self.sprite_group.draw(self.app.screen)
+
