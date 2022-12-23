36f8eb93e31b952101df5c6badd226d7fef17037
Paul Thomas <paulthomas2107@gmail.com>
2022-12-23 14:24:02 +0000

    More....

--- tetromino.py
+++ tetromino.py
@@ -1,22 +1,35 @@
 from settings import *
+import random
 
 
 class Block(pg.sprite.Sprite):
     def __init__(self, tetromino, pos):
         self.tetromino = tetromino
+        self.pos = vec(pos) + INIT_POS_OFFSET
 
         super().__init__(tetromino.tetris.sprite_group)
         self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill('orange')
-
         self.rect = self.image.get_rect()
-        self.rect.topleft = pos[0] * TILE_SIZE, pos[1] * TILE_SIZE
+
+    def set_rect_pos(self):
+        self.rect.topleft = self.pos * TILE_SIZE
+
+    def update(self):
+        self.set_rect_pos()
 
 
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
-        Block(self, (4, 7))
+        self.shape = random.choice(list(TETROMINOES.keys()))
+        self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
+
+    def move(self, direction):
+        move_direction = MOVE_DIRECTIONS[direction]
+        for block in self.blocks:
+            block.pos += move_direction
 
     def update(self):
-        pass
+        self.move(direction='down')
+        pg.time.wait(200)
